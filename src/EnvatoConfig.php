12039e510c74c8dd5e15ca500f970795a83c6457
Chauncey McAskill <chauncey@mcaskill.ca>
2023-02-23 15:01:45 -0500

    Add support for loading personal token from environment variable or `.env` file (#14)
    * Add support for loading personal token from environment variable
    
    The config class will look for `ENVATO_TOKEN`.
    
    * Add support for loading personal token from `.env` file
    
    Plugin requires vlucas/phpdotenv v4 or v5.
    
    * Update Installation and Configuration instructions
    
    Changed:
    - Added notice that the plugin can be installed locally.
    - Improved instructions on configuring the plugin to propose either the local `composer.json` file or the global `config.json` file.
    - Added code block hint for environment variable.
    
    * Improve environment variable check in EnvatoConfig

--- src/EnvatoConfig.php
+++ src/EnvatoConfig.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace SzepeViktor\Composer\Envato;
 
 use Composer\Config;
+use Dotenv\Dotenv;
 
 class EnvatoConfig
 {
     public const ENVATO_CONFIG = 'envato';
+
+    public const ENV_VAR_TOKEN = 'ENVATO_TOKEN';
 
     /**
      * @var array<mixed>
@@ -24,6 +27,9 @@ class EnvatoConfig
     {
         $envatoConfig = $composerConfig->get(self::ENVATO_CONFIG);
         $this->config = \is_array($envatoConfig) ? $envatoConfig : [];
+
+        $this->loadDotenv();
+        $this->mergeEnvConfig();
 
         $this->valid = \array_key_exists('token', $this->config)
             && \is_string($this->config['token'])
@@ -62,5 +68,20 @@ class EnvatoConfig
             \array_keys($this->config['packages']),
             $this->config['packages']
         );
+    }
+
+    protected function loadDotenv(): void
+    {
+        $cwd = \getcwd();
+        if (\is_string($cwd) && \file_exists($cwd . DIRECTORY_SEPARATOR . '.env')) {
+            Dotenv::createImmutable($cwd)->safeLoad();
+        }
+    }
+
+    protected function mergeEnvConfig(): void
+    {
+        if (\array_key_exists(self::ENV_VAR_TOKEN, $_ENV)) {
+            $this->config['token'] = $_ENV[self::ENV_VAR_TOKEN];
+        }
     }
 }
